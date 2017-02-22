fcab04ff36ecd7619f06afbe6445b34155e3856f
Erik Cox <erikbcox@gmail.com>
2017-02-21 17:57:42 -0800

    Expose Challenge data as content provider and register the content provider in the manifest

--- app/src/main/java/rocks/ecox/dailyprogrammerchallenges/models/Challenge.java
+++ app/src/main/java/rocks/ecox/dailyprogrammerchallenges/models/Challenge.java
@@ -1,12 +1,14 @@
 package rocks.ecox.dailyprogrammerchallenges.models;
 
+import android.provider.BaseColumns;
+
 import com.activeandroid.Model;
 import com.activeandroid.annotation.Column;
 import com.activeandroid.annotation.Table;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-@Table(name = "Challenges")
+@Table(name = "Challenges", id = BaseColumns._ID)
 public class Challenge extends Model {
     @SerializedName("kind")
     @Expose
