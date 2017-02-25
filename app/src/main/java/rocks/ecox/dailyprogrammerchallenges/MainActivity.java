3428bf464d6f71913aa16b1375f37fccb9073548
Erik Cox <erikbcox@gmail.com>
2017-02-24 16:49:59 -0800

    Add Loader to MainActivity

--- app/src/main/java/rocks/ecox/dailyprogrammerchallenges/MainActivity.java
+++ app/src/main/java/rocks/ecox/dailyprogrammerchallenges/MainActivity.java
@@ -1,12 +1,18 @@
 package rocks.ecox.dailyprogrammerchallenges;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -15,9 +21,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import rocks.ecox.dailyprogrammerchallenges.data.DPChallengesContract;
 import rocks.ecox.dailyprogrammerchallenges.utility.UpdateChallenges;
+import timber.log.Timber;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor> {
+
+    // Constant for referring to a unique loader
+    private static final int CHALLENGE_LOADER_ID = 0;
+
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -33,6 +46,10 @@ public class MainActivity extends AppCompatActivity {
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
+
+    // Member variables for the adapter and RecyclerView
+    private CustomCursorAdapter mAdapter;
+    RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +68,17 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
+
+        // Set the RecyclerView to its corresponding view
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerViewChallenges);
+
+        // Set the layout for the RecyclerView to be a linear layout, which measures and
+        // positions items within a RecyclerView into a linear list
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Initialize the adapter and attach it to the RecyclerView
+        mAdapter = new CustomCursorAdapter(this);
+        mRecyclerView.setAdapter(mAdapter);
 
         // Get data from reddit and create Challenge objects
         UpdateChallenges.update();
@@ -77,6 +105,18 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * This method is called after this activity has been paused or restarted.
+     * This restarts the loader to re-query the underlying data for any changes.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Re-queries for all challenges
+        getSupportLoaderManager().restartLoader(CHALLENGE_LOADER_ID, null, this);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -97,6 +137,87 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Instantiates and returns a new AsyncTaskLoader with the given ID.
+     * This loader will return challenge data as a Cursor or null if an error occurs.
+     *
+     * Implements the required callbacks to take care of loading data at all stages of loading.
+     */
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, final Bundle loaderArgs) {
+
+        return new AsyncTaskLoader<Cursor>(this) {
+
+            // Initialize a Cursor, this will hold all the challenge data
+            Cursor mChallengeData = null;
+
+            // onStartLoading() is called when a loader first starts loading data
+            @Override
+            protected void onStartLoading() {
+                if (mChallengeData != null) {
+                    // Delivers any previously loaded data immediately
+                    deliverResult(mChallengeData);
+                } else {
+                    // Force a new load
+                    forceLoad();
+                }
+            }
+
+            // loadInBackground() performs asynchronous loading of data
+            @Override
+            public Cursor loadInBackground() {
+                // Will implement to load data
+
+                // Query and load all challenge data in the background; sort by priority
+
+                try {
+                    return getContentResolver().query(DPChallengesContract.ChallengeEntry.CONTENT_URI,
+                            null,
+                            null,
+                            null,
+                            DPChallengesContract.ChallengeEntry.COLUMN_DIFFICULTY);
+
+                } catch (Exception e) {
+                    Timber.d("Failed to asynchronously load data.");
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+
+            // deliverResult sends the result of the load, a Cursor, to the registered listener
+            public void deliverResult(Cursor data) {
+                mChallengeData = data;
+                super.deliverResult(data);
+            }
+        };
+
+    }
+
+    /**
+     * Called when a previously created loader has finished its load.
+     *
+     * @param loader The Loader that has finished.
+     * @param data The data generated by the Loader.
+     */
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        // Update the data that the adapter uses to create ViewHolders
+        mAdapter.swapCursor(data);
+    }
+
+
+    /**
+     * Called when a previously created loader is being reset, and thus
+     * making its data unavailable.
+     * onLoaderReset removes any references this activity had to the loader's data.
+     *
+     * @param loader The Loader that is being reset.
+     */
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mAdapter.swapCursor(null);
     }
 
     /**
