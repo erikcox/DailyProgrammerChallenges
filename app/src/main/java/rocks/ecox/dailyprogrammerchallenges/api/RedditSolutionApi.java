0b40a0a808865dba3ffcc9cc228e3590ac78dce0
Erik Cox <erikbcox@gmail.com>
2017-03-13 17:28:47 -0700

    Limit response depth to 1 for comments

--- app/src/main/java/rocks/ecox/dailyprogrammerchallenges/api/RedditSolutionApi.java
+++ app/src/main/java/rocks/ecox/dailyprogrammerchallenges/api/RedditSolutionApi.java
@@ -8,6 +8,6 @@ import retrofit.http.Path;
 import rocks.ecox.dailyprogrammerchallenges.models.Solution;
 
 public interface RedditSolutionApi {
-    @GET("/r/{sub}/comments/{id}.json?")
+    @GET("/r/{sub}/comments/{id}.json?depth=1")
     public void getFeed(@Path("sub") String sub, @Path("id") String id, Callback<List<Solution>> response);
 }
