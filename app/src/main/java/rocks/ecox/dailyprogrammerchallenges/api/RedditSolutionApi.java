65a0fddec411ca0f21b260149d07459dd69661ab
Erik Cox <erikbcox@gmail.com>
2017-03-13 17:25:43 -0700

    Got a response from the solution data

--- app/src/main/java/rocks/ecox/dailyprogrammerchallenges/api/RedditSolutionApi.java
+++ app/src/main/java/rocks/ecox/dailyprogrammerchallenges/api/RedditSolutionApi.java
@@ -1,12 +1,13 @@
 package rocks.ecox.dailyprogrammerchallenges.api;
 
+import java.util.List;
+
 import retrofit.Callback;
 import retrofit.http.GET;
 import retrofit.http.Path;
-import rocks.ecox.dailyprogrammerchallenges.models.Challenge;
 import rocks.ecox.dailyprogrammerchallenges.models.Solution;
 
 public interface RedditSolutionApi {
     @GET("/r/{sub}/comments/{id}.json?")
-    public void getFeed(@Path("sub") String sub, @Path("id") String id, Callback<Solution> response);
+    public void getFeed(@Path("sub") String sub, @Path("id") String id, Callback<List<Solution>> response);
 }
--- app/src/main/java/rocks/ecox/dailyprogrammerchallenges/utility/UpdateSolutions.java
+++ app/src/main/java/rocks/ecox/dailyprogrammerchallenges/utility/UpdateSolutions.java
@@ -1,17 +1,11 @@
 package rocks.ecox.dailyprogrammerchallenges.utility;
 
-import android.os.Build;
-import android.text.Html;
-
-import com.activeandroid.util.SQLiteUtils;
-import com.crashlytics.android.Crashlytics;
 import com.facebook.stetho.okhttp3.StethoInterceptor;
 import com.google.gson.GsonBuilder;
 
 import java.lang.reflect.Modifier;
 import java.util.List;
 
-import io.fabric.sdk.android.Fabric;
 import okhttp3.OkHttpClient;
 import retrofit.Callback;
 import retrofit.RestAdapter;
@@ -19,7 +13,6 @@ import retrofit.RetrofitError;
 import retrofit.client.Response;
 import retrofit.converter.GsonConverter;
 import rocks.ecox.dailyprogrammerchallenges.api.RedditSolutionApi;
-import rocks.ecox.dailyprogrammerchallenges.models.ChildComment;
 import rocks.ecox.dailyprogrammerchallenges.models.Solution;
 import timber.log.Timber;
 
@@ -47,47 +40,55 @@ public class UpdateSolutions {
         final RedditSolutionApi redditSolutions = restAdapter.create(RedditSolutionApi.class);
 
         // Get solution json data
-        redditSolutions.getFeed(subreddit, challenge, new Callback<Solution>() {
+        redditSolutions.getFeed(subreddit, challenge, new Callback<List<Solution>>() {
             @Override
-            public void success(Solution solution, Response response) {
-                for (ChildComment c : solution.getData().getChildren()) {
-                    try {
-                        // Check if solution already exists in DB
-                        List<Solution> duplicateSolutions =
-                                SQLiteUtils.rawQuery(Solution.class,
-                                        "SELECT * FROM Solutions WHERE parent_id = ?",
-                                        new String[] {c.getData().getCommentParentId() });
-
-                        if (duplicateSolutions.size() == 0) {
-                            // Set solution attributes
-                            Solution sol = new Solution();
-
-                            sol.setCommentParentId(c.getData().getCommentParentId());
-                            sol.setCommentId(c.getData().getCommentId());
-                            sol.setCommentText(c.getData().getCommentText());
-
-                            if(c.getData().getCommentTextHtml() != null) {
-                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                                    sol.setCommentTextHtml(Html.fromHtml(c.getData().getCommentTextHtml(), Html.FROM_HTML_MODE_COMPACT).toString());
-                                } else {
-                                    sol.setCommentTextHtml(Html.fromHtml(c.getData().getCommentTextHtml()).toString());
-                                }
-                            }
-
-                            sol.setCommentUps(c.getData().getCommentUps());
-
-                            // TODO: add logic for showComment boolean
-
-                            sol.save();
-                        }
-                    } catch (NullPointerException e) {
-                        // Check if Crashlytics is running before logging exception
-                        if (Fabric.isInitialized()) {
-                            Crashlytics.logException(e);
-                        }
-                        e.printStackTrace();
+            public void success(List<Solution> solutions, Response response) {
+                for (Solution sol : solutions) {
+                    if (sol.getData().getChildren().size() > 0 && sol.getData().getChildren().get(0).getData().getCommentParentId() != null) {
+                        Timber.d("Hello!! %s | ", sol.getData().getChildren().get(0).getData().getCommentText());
                     }
                 }
+
+                Timber.d("Hello!! %s | ", postId);
+//                for (ChildComment c : solutions.getData().getChildren()) {
+//                    try {
+//                        // Check if solution already exists in DB
+//                        List<Solution> duplicateSolutions =
+//                                SQLiteUtils.rawQuery(Solution.class,
+//                                        "SELECT * FROM Solutions WHERE parent_id = ?",
+//                                        new String[] {c.getData().getCommentParentId() });
+//
+//                        if (duplicateSolutions.size() == 0) {
+//                            // Set solution attributes
+//                            Solution sol = new Solution();
+//
+//                            sol.setCommentParentId(c.getData().getCommentParentId());
+//                            sol.setCommentId(c.getData().getCommentId());
+//                            sol.setCommentText(c.getData().getCommentText());
+//
+//                            if(c.getData().getCommentTextHtml() != null) {
+//                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+//                                    sol.setCommentTextHtml(Html.fromHtml(c.getData().getCommentTextHtml(), Html.FROM_HTML_MODE_COMPACT).toString());
+//                                } else {
+//                                    sol.setCommentTextHtml(Html.fromHtml(c.getData().getCommentTextHtml()).toString());
+//                                }
+//                            }
+//
+//                            sol.setCommentUps(c.getData().getCommentUps());
+//
+//                            // TODO: add logic for showComment boolean
+//
+//                            sol.save();
+//
+//                        }
+//                    } catch (NullPointerException e) {
+//                        // Check if Crashlytics is running before logging exception
+//                        if (Fabric.isInitialized()) {
+//                            Crashlytics.logException(e);
+//                        }
+//                        e.printStackTrace();
+//                    }
+//                }
 
             }
 
