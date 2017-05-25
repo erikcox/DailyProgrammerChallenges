f6cdbe8eb1c324f703c1001416581b25bc4806f8
Erik Cox <erikbcox@gmail.com>
2017-05-24 20:16:25 -0700

    Add getActivity to widget

--- app/src/main/java/rocks/ecox/dailyprogrammerchallenges/widget/DPChallengeWidgetProvider.java
+++ app/src/main/java/rocks/ecox/dailyprogrammerchallenges/widget/DPChallengeWidgetProvider.java
@@ -1,8 +1,11 @@
 package rocks.ecox.dailyprogrammerchallenges.widget;
 
+import android.app.Activity;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
 import rocks.ecox.dailyprogrammerchallenges.R;
@@ -28,7 +31,14 @@ public class DPChallengeWidgetProvider extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.dpchallenge_widget_provider);
+            Intent configIntent = new Intent(context, Activity.class);
+
+            PendingIntent configPendingIntent = PendingIntent.getActivity(context, 0, configIntent, 0);
+
+            remoteViews.setOnClickPendingIntent(R.id.widget, configPendingIntent);
+            appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
+
         }
     }
 
